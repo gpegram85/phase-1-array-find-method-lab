220d9b3bb2fe2018e8682222e57ffb740af063c6
Grey Pegram <dpegram85@gmail.com>
2024-01-14 11:23:57 -0500

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {                                     //takes in an array (doesn't have to be exact)
+   let winningYear = record.find((record) => record.result === 'W') //returns a boolean
+                                                                    //use the boolean value to write an if stmt
+    if(winningYear) {
+        return winningYear.year
+    } else {
+        return undefined
+    }
+}
